473bed522f6ac8229dfa3bea929834dead90b5ce
perov-alexey <perov-alexey@yandex.ru>
2017-11-12 03:25:53 +0300

    Add taking commissions info from server

--- src/main/webapp/scripts/commission-page-module.js
+++ src/main/webapp/scripts/commission-page-module.js
@@ -1,9 +1,20 @@
 (function(angular) {
     'use strict';
-    angular.module('commissionPage', ['ngRoute']).controller('commissionPageController', function($scope) {
-        $scope.commissionsNumber = 5;
+    angular.module('commissionPage', ['ngRoute']).controller('commissionPageController', function($scope, $http) {
+
+        $scope.slotsAmount = 5;
+
+        $http.get("/commission/all")
+            .then(function(response) {
+               $scope.commissions = response.data;
+               for (var index = response.data.length; index < $scope.slotsAmount; index++) {
+                   $scope.commissions.push({});
+               }
+            });
+
         $scope.getNumberRange = function(num) {
             return new Array(num);
-        }
+        };
+
     });
 })(window.angular);
